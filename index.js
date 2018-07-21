32a239ce2074b182a3b529a10dd54a12bae40bce
mariolatiffathy <32080032+mariolatiffathy@users.noreply.github.com>
2018-07-21 21:30:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on('message', msg => {
     if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'help') return msg.channel.send('_Our Bot is Under Maintenance_');
+    if (command === 'help') return msg.author.sendMessage("_Our Bot is under Maintenance_")
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
 
