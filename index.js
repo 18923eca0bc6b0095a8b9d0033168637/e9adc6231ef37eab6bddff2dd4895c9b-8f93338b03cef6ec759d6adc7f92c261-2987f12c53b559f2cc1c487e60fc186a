42bf2779a4a5b237faeb351c660158f77860d9d2
mariolatiffathy <32080032+mariolatiffathy@users.noreply.github.com>
2018-07-24 16:19:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -174,14 +174,12 @@ const commands = {
 	}
 };
 
-client.on('ready', () => {
-	
-	let activities_list = [
+const activities_list = [
 		"Use %help", 
-		"on " + client.guilds.size + " servers",
 		"Bot by MarioLatifFathy"
 	]; // creates an arraylist containing phrases you want your bot to switch through.
-	
+
+client.on('ready', () => {
 	setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
         client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
